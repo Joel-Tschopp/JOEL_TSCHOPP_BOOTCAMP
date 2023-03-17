46b127d77a14b83b916aa2473b83aeab47d61384
Joel-Tschopp <joel.tschopp@ajila.com>
2023-03-17 16:25:42 +0100

    Update AHV autocomplete func

--- apps/src/main/content/jcr_root/apps/ajila-forms-customers/ajila-forms-joeltschopp/clientlibs/common/js/root.js
+++ apps/src/main/content/jcr_root/apps/ajila-forms-customers/ajila-forms-joeltschopp/clientlibs/common/js/root.js
@@ -33,132 +33,67 @@ window.ajila.forms.joeltschopp.common = window.ajila.forms.joeltschopp.common ||
 
 
     /**
-     * Simple Solution to autocomplete AHV number. Isn't completely working. Can't paste and it's possible to change the formatation.
-     *
-     context.formatAhvNumber = function (ahvTextbox) {
-        var key;
-        var oldNr = "";
-        $("." + ahvTextbox.name).find("input").keydown(function(event){
-            key = event.key;
-        });
-
-        $("." + ahvTextbox.name).find("input").on("input", function () {
-            var ahvNr = this.value;
-            if (key <= 9 && key >= 0) {
-                if (ahvNr.length == 3) {
-                    this.value = this.value + ".";
-                } else if (ahvNr.length == 8) {
-                    this.value = this.value + ".";
-                } else if (ahvNr.length == 13) {
-                    this.value = this.value + ".";
-                } else if (ahvNr.length > 16) {
-                    this.value = this.value.substring(0, 16);
-                }
-            } else {
-                if (key != "Backspace" && key != "Delete") {
-                    this.value = oldNr;
-                }
-            }
-            oldNr = this.value;
-        });
-    }*/
-
-
-    /**
-     * Simple Solution to autocomplete AHV number. it's possible to change the formatation. Pasting is possible.
-     *
-    context.formatAhvNumber = function (ahvTextbox) {
-        var key;
-        var oldNr = "";
-
-        function containsOnlyNumbers(str) {
-            return /^[\.0-9]*$/.test(str);
-        }
-
-        function isPossibleAHVNr(str) {
-            return /^756.[0-9]{4}.[0-9]{4}.[0-9]{2}/.test(str);
-        }
-
-        $("." + ahvTextbox.name).find("input").keydown(function (event) {
-            key = event.key;
-        });
-
-        $("." + ahvTextbox.name).find("input").on("input", function () {
-            if (key <= 9 && key >= 0) {
-                if (this.value.length == 3 || this.value.length == 8 || this.value.length == 13) {
-                    this.value = this.value + ".";
-                } else if (this.value.length > 16) {
-                    this.value = this.value.substring(0, 16);
-                }
-            } else {
-                if (key != "Backspace" && key != "Delete") {
-                    if (containsOnlyNumbers(this.value) == false) {
-                        this.value = oldNr;
-                    } else {
-                        this.value = this.value.substring(0, 16);
-                    }
-                }
-            }
-            oldNr = this.value;
-        });
-
-        $("." + ahvTextbox.name).find("input").focusout(function () {
-            if (isPossibleAHVNr(oldNr) == true) {
-                console.log(oldNr + " is a correct AHV-nr");
-            } else {
-                console.log(oldNr + " is not a correct AHV-nr");
-            }
-        })
-    }*/
-
-
-    /**
-     * Complex Solution to autocomplete AHV number. Checking every input
+     * Solution to autocomplete AHV number.
      */
     context.formatAhvNumber = function (ahvTextbox) {
+        // contains the key the user pressed
         var key;
+        // contains the old AHV-nr which validated correctly
         var oldNr = "";
 
+        // Tests if a string contains only numbers and dots
         function containsOnlyNumbers(str) {
             return /^[\.0-9]*$/.test(str);
         }
 
+        // Tests if a string is a correct AHV-nr
         function isPossibleAHVNr(str) {
-            return /^756.[0-9]{4}.[0-9]{4}.[0-9]{2}/.test(str);
+            return /756[.]\d{4}[.]\d{4}[.]\d{2}$/.test(str);
         }
 
+        // get the amount of dots in string
+        function getDotCount(str) {
+            return str.split(".").length - 1;
+        }
+
+        // when the user presses a key, the value of the key get written into key var
         $("." + ahvTextbox.name).find("input").keydown(function (event) {
             key = event.key;
         });
 
+        // after every key press this event get called. Value contains value from the Input. This logic is on this event because the keydown was to slow.
         $("." + ahvTextbox.name).find("input").on("input", function () {
+            // Checking if the entered key is a single number
             if (key <= 9 && key >= 0) {
-                if (this.value.length == 3 || this.value.length == 8 || this.value.length == 13) {
+                // Write a dot after the correct length of the string is reached.
+                if (this.value.length == 3 || this.value.length == 8 || this.value.length == 13 && getDotCount(this.value) <= 3)
                     this.value = this.value + ".";
-                } else if (this.value.length > 16) {
+                else if (this.value.length > 16)
                     this.value = this.value.substring(0, 16);
-                }
             } else {
+                // When it's not a single number it get checked if the clicked key is backspace or delete. Because they have different treatment.
                 if (key != "Backspace" && key != "Delete") {
-                    if (containsOnlyNumbers(this.value) == false) {
+                    // Checks if user pasted a string that contains only numbers. Single tapped letter keys get validated here too.
+                    if (containsOnlyNumbers(this.value) == false)
                         this.value = oldNr;
-                    } else {
+                    else {
                         this.value = this.value.substring(0, 16);
+                        // Check if there are already 3 dots
+                        if (getDotCount(this.value) > 3)
+                            this.value = oldNr;
                     }
                 }
             }
             oldNr = this.value;
         });
 
+        // Event fires when user leaves the Input
         $("." + ahvTextbox.name).find("input").focusout(function () {
-            if (isPossibleAHVNr(oldNr) == true) {
+            if (isPossibleAHVNr(oldNr) == true)
                 console.log(oldNr + " is a correct AHV-nr");
-            } else {
+            else
                 console.log(oldNr + " is not a correct AHV-nr");
-            }
         })
     }
-
-
 }(window.ajila.forms.joeltschopp.common));
 
